23ba4f6e9dd3897aa31d161ff39ee00a3998d406
Alvaro Marfer <alvaro.eotec@gmail.com>
2025-02-07 22:00:37 +0100

    Deletes js files

--- src/routes/photos.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.photosRouter = void 0;
-// src/routes/photos.routes.ts
-const express_1 = require("express");
-const photos_controller_1 = require("../controllers/photos.controller");
-exports.photosRouter = (0, express_1.Router)();
-exports.photosRouter.get('/get-photo-url', photos_controller_1.getPhotoUrl);
-exports.photosRouter.get('/get-photo', photos_controller_1.getPhoto);
